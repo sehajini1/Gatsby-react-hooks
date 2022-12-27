287875a2f5162938bd353073cbc9dc533ade9d15
Sehajini <jayakodyjast.19@uom.lk>
2022-12-27 17:30:07 +0530

    global style

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,12 +9,13 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Design+Code`,
+    description: `Don’t skip design. Learn design and code by building real apps with React and Swift. Complete courses about the best tools.`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -1,6 +1,7 @@
 import React from "react"
-import { Link } from "gatsby"
 import styled from "styled-components"
+import { themes } from "../styles/ColorStyles"
+import { H1, MediumText } from "../styles/TextStyles"
 
 function HeroSection() {
     return (
@@ -36,13 +37,11 @@ const TextWrapper = styled.div`
     gap: 30px;
 `
 
-const Title = styled.h1`
-    font-weight: bold;
-    font-size: 60px;
-    color: white;
+const Title = styled(H1)`
+    color: ${themes.dark.text1};
 `
 
-const Description = styled.p`
+const Description = styled(MediumText)`
     font-size: 17px;
     line-height: 130%;
 `
