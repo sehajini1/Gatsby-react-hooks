f4448998ab731eba78f4bc6b9e2448bc0c5d39da
Sehajini <jayakodyjast.19@uom.lk>
2022-12-28 12:48:23 +0530

    Backdrop-filter blur

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -1,5 +1,6 @@
 import React from "react"
 import styled from "styled-components"
+import MockupAnimation from "../animations/MockupAnimation"
 import PurchaseButton from "../button/PurchaseButton"
 import { themes } from "../styles/ColorStyles"
 import { H1, MediumText } from "../styles/TextStyles"
@@ -19,6 +20,7 @@ function HeroSection() {
                         subtitle="128+ hours of video"
                     />
                 </TextWrapper>
+                <MockupAnimation/>
             </ContentWrapper>
         </Wrapper>
     )
@@ -28,12 +30,15 @@ export default HeroSection
 
 const Wrapper = styled.div`
     background: linear-gradient(180deg, #4316db 0%, #9076e7 100%);
+    overflow: hidden;
 `
 
 const ContentWrapper = styled.div`
     max-width: 1234px;
     margin: 0 auto;
     padding: 200px 30px;
+    display: grid;
+    grid-template-columns: 368px auto;
 `
 
 const TextWrapper = styled.div`
